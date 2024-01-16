90ca1720de7cd2596c07b3de667737161b0b68b1
Andres Parra <byandrev@gmail.com>
2024-01-15 21:17:08 -0500

    create Button component with different colorShemes

---
+++ src/types/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  primary: { bg: "bg-primary-400", text: "text-text.text" },
+  default: { bg: "bg-bg-alt", text: "text-text.text" },
+  white: { bg: "bg-[#fff]", text: "text-[#222]" },
+  black: { bg: "bg-[#111]", text: "text-text.text" },
+  warning: { bg: "bg-yellow-600", text: "text-text.text" },
+  error: { bg: "bg-red-600", text: "text-text.text" },
+};
