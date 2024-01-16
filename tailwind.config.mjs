172b87895e1857e8c3a6df9e7829a66d233ddbe5
Andres Parra <byandrev@gmail.com>
2024-01-15 19:23:59 -0500

    design Newsletter component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,10 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#6366f1",
+        primary: {
+          100: "#6366f1",
+          200: "#8083f2",
+          300: "#686bed",
+          400: "#6366f1",
+          500: "#5053d9",
+          600: "#4144ba",
+        },
         border: "#282e3d",
         text: "#fff",
         textAlt: "rgb(107 114 128)",
+        "bg-base": "#1e2432",
+        "bg-alt": "#282d43",
+        "bg-dark": "#171c24",
+        "bg-black": "#0f1326",
       },
       fontFamily: {
         heading: "'Jost Variable', sans-serif",
