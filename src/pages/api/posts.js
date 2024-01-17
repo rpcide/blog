2a39f76c947d6f00a187039ce0b473c4d5b106bc
Andres Parra <byandrev@gmail.com>
2024-01-16 21:45:51 -0500

    implement pagination posts

--- src/pages/api/posts.js
+++ src/pages/api/posts.js
@@ -1,16 +1,19 @@
 import { getAllPosts } from "../../lib/Posts";
 
-export const get = async () => {
-  const { posts } = await getAllPosts({ page: 1 });
+export const GET = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const page = parseInt(url.searchParams.get("page"));
 
-  if (!posts) {
+  const data = await getAllPosts({ page: page || 1 });
+
+  if (!data) {
     return new Response(null, {
       status: 404,
       statusText: "Not found",
     });
   }
 
-  return new Response(JSON.stringify(posts), {
+  return new Response(JSON.stringify(data), {
     status: 200,
   });
 };
